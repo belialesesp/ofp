1d06b420cbc82598c53875f83f6f948a277b96d7
belialesesp <priscilla.ferreira@esesp.es.gov.br>
2026-03-01 11:14:22 -0300

    [code] error handling

--- search.php
+++ search.php
@@ -14,7 +14,10 @@ get_header();
 				<h1 class="page-title">
 					<?php
 					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'our-family-passport' ), '<span>' . get_search_query() . '</span>' );
+					printf(
+						esc_html__( 'Search Results for: %s', 'our-family-passport' ),
+						'<span>' . esc_html( get_search_query() ) . '</span>'
+					);
 					?>
 				</h1>
 			</header><!-- .page-header -->
@@ -23,11 +26,6 @@ get_header();
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
 				get_template_part( 'template-parts/content', 'search' );
 			endwhile;
 			?>
